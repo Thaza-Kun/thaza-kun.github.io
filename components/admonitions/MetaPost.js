0032b0bd20941f94b56e5217bdccf28dfb55fb1f
Thaza_Kun <61819672+Thaza-Kun@users.noreply.github.com>
2022-02-16 02:17:00 +0800

    Added MetaPost.js

---
+++ components/admonitions/MetaPost.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import _ from 'lodash'
+
+// Force commit rename
+
+export default function MetaPost(props) {
+  return (
+    <aside className="prose mx-2 my-6 max-w-none rounded-lg border-4 border-teal-600 px-4 dark:prose-dark">
+      <div>{props.children}</div>
+    </aside>
+  )
+}
