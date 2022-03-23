59ae8f2c47a283c39da4699f55a3a6c55f4d8671
Thaza_Kun <61819672+Thaza-Kun@users.noreply.github.com>
2022-03-23 15:27:11 +0800

    Changed metapost styling

--- components/admonitions/MetaPost.js
+++ components/admonitions/MetaPost.js
@@ -5,8 +5,12 @@ import _ from 'lodash'
 
 export default function MetaPost(props) {
   return (
-    <aside className="prose mx-2 my-6 max-w-none rounded-lg border-4 border-teal-600 px-4 dark:prose-dark">
-      <div>{props.children}</div>
-    </aside>
+    <div className="group prose relative my-4 max-w-none rounded-lg dark:prose-dark">
+      {/* <div className="rounded-lg bg-gradient-to-b from-green-600 to-green-300 bg-clip-border py-2"> */}
+      <div className="rounded-2xl rounded-l-none border-l-8 border-green-500 bg-gray-200 py-0.5 px-4 dark:bg-gray-800">
+        {props.children}
+      </div>
+      {/* </div> */}
+    </div>
   )
 }
