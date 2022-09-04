ab9bd3bac872b01c3643dffffeded5dbbaa8b629
Thaza_Kun <61819672+Thaza-Kun@users.noreply.github.com>
2022-09-04 13:29:56 +0800

    Show current projects from toml file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,40 @@
 import React from "react";
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, Text, Heading } from "@chakra-ui/react";
 
 import { Header, Main, Cards, Footer } from "@components";
 import { DefaultLayout } from "src/layouts";
 
-const Home: React.FC = () => {
+import { getData, ProjectDataProps, ProjectProps } from "data/profile";
+import { GetStaticProps } from "next";
+
+interface HomeProps {
+    projects: ProjectProps[];
+}
+
+const Home: React.FC<HomeProps> = (props) => {
+    // Show only current projects
+    const projects: ProjectProps[] = props.projects.filter((project) => {
+        return !!!project.date.end;
+    });
     return (
         <DefaultLayout>
             <Flex direction="column" minH="100vh">
                 <Main />
-                <Cards />
+                <Heading as="h3">Karya Terkini</Heading>
+                <Cards data={projects} />
+                {/* TODO Iklan:
+                - buku
+                - Samudra
+                - Kofi
+                */}
             </Flex>
         </DefaultLayout>
     );
+};
+
+export const getStaticProps: GetStaticProps = () => {
+    const projects: ProjectProps[] = getData("projects").projects;
+    return { props: { projects } };
 };
 
 export default Home;
