9c3322dd1aa39fc01a9f00897c2a6ad611f85811
Thaza_Kun <61819672+Thaza-Kun@users.noreply.github.com>
2022-10-06 11:15:22 +0800

    :sparkles: NEW: Migrated to Quarto

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { NavBar } from "./header";
-export { Logo } from "./logo";
-export { Main } from "./main";
-export { Button } from "./button";
-export { CardGrid } from "./cards";
-export { Footer } from "./footer";
